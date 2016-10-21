bed1a5b8dac79fe639643af5510ef239de892334
Nikita Sapunov <kiton1994@gmail.com>
2016-10-21 23:16:02 +0300

    Util works

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # 11_duplicates
+
+Утилита для поиска файлов-дупликатов в директории.
+
+```{r, engine='bash'}
+$ python duplicates.py --help
+usage: duplicates.py [-h] -d DIRECTORY
+
+Find duplicate files in DIRECTORY
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DIRECTORY, --directory DIRECTORY
+                        Directory to find duplicates
+```
+
+**Что считается дупликатом:** файлы, которые имеют одинаковые имена и размеры
+
+> 11 задача с сайта [devman.org](https://devman.org/) - решай задачи и читай учебный материал
+
+
+## Использование
+
+```{r, engine='bash'}
+$ python duplicates.py -d ~/Documents/
+Duplicates founded: 2
+---------------------
+* /home/nsapunov/Documents/СКОРОЧТЕНИЕ.png
+* /home/nsapunov/Documents/BEC Vantage Testbuilder/СКОРОЧТЕНИЕ.png
+---------------------
+* /home/nsapunov/Documents/1 занятие. тестировани.odt
+* /home/nsapunov/Documents/xmind/1 занятие. тестировани.odt
+```
+
+> Скрипт использует python 3 версии.
+
+
+## Лицензия
+
+[Creative Commons Attribution License](http://creativecommons.org/licenses/by/2.0/)
--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,62 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+from hashlib import sha1
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def _create_duplicates_tree(directory):
+    tree = {}
+
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+
+            try:
+                stat = os.stat(filepath)
+            except FileNotFoundError:
+                continue
+
+            file_hash = sha1(
+                bytes("%s-%s" % (filename, stat.st_size), "utf-8")
+            ).hexdigest()
+
+            if tree.get(file_hash, None) is None:
+                tree[file_hash] = [filepath]
+            else:
+                tree[file_hash].append(filepath)
+
+    return tree
+
+
+def get_duplicates(directory):
+    dup_tree = _create_duplicates_tree(directory)
+
+    return list(filter(lambda it: len(it) > 1, dup_tree.values()))
+
+
+def main():
+    description = "Find duplicate files in DIRECTORY"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        "-d", "--directory",
+        type=str,
+        required=True,
+        help="Directory to find duplicates"
+    )
+
+    args = parser.parse_args()
+    duplicates = get_duplicates(args.directory)
+    count_duplicates = len(duplicates)
+    line_width = len(str(count_duplicates)) + 20
+
+    print("Duplicates founded: {}".format(count_duplicates))
+    for items in duplicates:
+        print("-" * line_width)
+        for item in items:
+            print("* " + item)
 
 
 if __name__ == '__main__':
-    pass
+    main()
